a80ac52663ab66a26c2fc1bf0a65c94345f5012a
mousasbbah <mosassbbah@gmail.com>
2021-04-19 19:07:41 +0300

    uncomment ssl

--- puplic/js/solar.js
+++ puplic/js/solar.js
@@ -1,7 +1,7 @@
 $num = $('.my-card').length;
 $even = $num / 2;
 $odd = ($num + 1) / 2;
- 
+
 if ($num % 2 == 0) {
   $('.my-card:nth-child(' + $even + ')').addClass('active');
   $('.my-card:nth-child(' + $even + ')').prev().addClass('prev');
@@ -11,26 +11,26 @@ if ($num % 2 == 0) {
   $('.my-card:nth-child(' + $odd + ')').prev().addClass('prev');
   $('.my-card:nth-child(' + $odd + ')').next().addClass('next');
 }
- 
-$('.my-card').click(function() {
+
+$('.my-card').hover(function() {
   $slide = $('.active').width();
   console.log($('.active').position().left);
-  
+
   if ($(this).hasClass('next')) {
-    $('.card-carousel').stop(false, true).animate({left: '-=' + $slide});
+    $('.card-carousel').stop(false, true).animate({left: '-=' + $slide},1100);
   } else if ($(this).hasClass('prev')) {
-    $('.card-carousel').stop(false, true).animate({left: '+=' + $slide});
+    $('.card-carousel').stop(false, true).animate({left: '+=' + $slide},1100);
   }
-  
   $(this).removeClass('prev next');
   $(this).siblings().removeClass('prev active next');
-  
+
   $(this).addClass('active');
   $(this).prev().addClass('prev');
   $(this).next().addClass('next');
+
 });
- 
- 
+
+
 // Keyboard nav
 $('html body').keydown(function(e) {
   if (e.keyCode == 37) { // left
